dacfe36e4cc3cf72aa4c26ed107e9069d1a5af84
reduan-azouaghe <reduan.azouaghe@no.experis.com>
2025-09-03 11:10:47 +0200

    Github Workflow: Test on PR

--- exercise.wwwapi/Configuration/ConfigurationSettings.cs
+++ exercise.wwwapi/Configuration/ConfigurationSettings.cs
@@ -5,7 +5,9 @@
         IConfiguration _configuration;
         public ConfigurationSettings()
         {
-            _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.Debug.json", optional: true).Build();
         }
         public string GetValue(string key)
         {
