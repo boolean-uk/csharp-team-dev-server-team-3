652e0631263065ce594a28fda012805a63927001
Oyvind Timian Dokk Husveg <oyvind.timian.dokk.husveg@no.experis.com>
2025-09-03 12:41:45 +0200

    Misc

--- exercise.wwwapi/Models/User.cs
+++ exercise.wwwapi/Models/User.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace exercise.wwwapi.Models
 {
-    public enum Roles { teacher, student } // Changed to public
+    public enum Roles { teacher, student }
     [Table("users")]
     public class User
     {
