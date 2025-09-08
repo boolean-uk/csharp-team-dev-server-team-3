5dac0643a8ba5b2a401f0df5f05c0c69ab67f20a
Oyvind Timian Dokk Husveg <oyvind.timian.dokk.husveg@no.experis.com>
2025-09-08 14:47:15 +0200

    Added default error for invalid endpoint and made tests for that functionality

---
+++ exercise.tests/IntegrationTests/EndpointStatusTest.cs
@@ -0,0 +1,85 @@
+﻿using exercise.wwwapi.DTOs.Login;
+using exercise.wwwapi.DTOs.Register;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace exercise.tests.IntegrationTests
+{
+    [TestFixture]
+    public class EndpointStatusTest
+    {
+        private WebApplicationFactory<Program> _factory;
+        private HttpClient _client;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Arrange 
+            _factory = new WebApplicationFactory<Program>();
+            _client = _factory.CreateClient();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _client.Dispose();
+            _factory.Dispose();
+        }
+
+        [Test] 
+        public async Task PostCheckInvalidEndpoint()
+        {
+            var body = new { 
+                email= $"myemailVery@gmail.com",
+                password = "someR21!password"
+            };
+            var json = JsonSerializer.Serialize(body);
+            var requestBody = new StringContent(json, Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await _client.PostAsync("/okokoNONEVER", requestBody);
+
+            var contentString = await response.Content.ReadAsStringAsync();
+
+            JsonNode? message = null;
+            if (!string.IsNullOrWhiteSpace(contentString))
+            {
+                message = JsonNode.Parse(contentString);
+            }
+
+            var messageText = message?["message"]?.GetValue<string>();
+
+            Console.WriteLine("Message: " + message);
+            // Assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+            Assert.That(message?["message"]?.GetValue<string>(), Is.EqualTo("Endpoint not found"));
+        }
+
+        [Test]
+        public async Task GetCheckInvalidEndpoint()
+        {
+
+            // Act
+            var response = await _client.GetAsync("/okokoNONEVER");
+
+            var contentString = await response.Content.ReadAsStringAsync();
+
+            JsonNode? message = null;
+            if (!string.IsNullOrWhiteSpace(contentString))
+            {
+                message = JsonNode.Parse(contentString);
+            }
+
+            var messageText = message?["message"]?.GetValue<string>();
+
+            Console.WriteLine("Message: " + message);
+            // Assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+            Assert.That(message?["message"]?.GetValue<string>(), Is.EqualTo("Endpoint not found"));
+        }
+    }
+}
