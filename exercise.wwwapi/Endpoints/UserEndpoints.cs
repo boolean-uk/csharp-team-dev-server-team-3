73db51de9ceeb252eba1038239a67b8801df1e18
Hans Jakob Håland <Hans.Jakob.Haland@no.experis.com>
2025-09-10 15:47:01 +0200

    Specifies response.Data for Register response body

--- exercise.wwwapi/Endpoints/UserEndpoints.cs
+++ exercise.wwwapi/Endpoints/UserEndpoints.cs
@@ -83,7 +83,7 @@ namespace exercise.wwwapi.EndPoints
                 Data = mapper.Map<UserDTO>(user)
             };
 
-            return Results.Created($"/users/{response.Data.id}", response);
+            return Results.Created($"/users/{response.Data.id}", response.Data);
         }
 
         [ProducesResponseType(StatusCodes.Status200OK)]
