53dd7d0db73613eea0df47dd69dfd91773b7692e
Oyvind Timian Dokk Husveg <oyvind.timian.dokk.husveg@no.experis.com>
2025-09-10 15:23:52 +0200

    Created models for post and postcomment, and some DTOs

---
+++ exercise.wwwapi/DTOs/GetUsers/UserBasicDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace exercise.wwwapi.DTOs.GetUsers
+{
+    public class UserBasicDTO
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Photo {  get; set; }
+    }
+}
---
+++ exercise.wwwapi/DTOs/Posts/PostCommentDTO.cs
@@ -0,0 +1,16 @@
+﻿using exercise.wwwapi.DTOs.GetUsers;
+using exercise.wwwapi.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise.wwwapi.DTOs.Posts
+{
+    public class PostCommentDTO
+    {
+        public int Id { get; set; }
+        public UserBasicDTO? User { get; set; }
+        public required string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
