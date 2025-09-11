7572816a0bb3de93eb47cbce9f82355c45d8709a
Oyvind Timian Dokk Husveg <oyvind.timian.dokk.husveg@no.experis.com>
2025-09-11 10:58:17 +0200

    Seeder for posts works

---
+++ exercise.wwwapi/Data/PostCommentData.cs
@@ -0,0 +1,106 @@
+﻿using exercise.wwwapi.Models;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace exercise.wwwapi.Data
+{
+    public class PostCommentData
+    {
+        private List<string> _commentStarts = new List<string>
+        {
+            "Wow!",
+            "That sounds amazing",
+            "I love it",
+            "So cool",
+            "Congrats",
+            "Looks fun",
+            "Nice!",
+            "That’s awesome",
+            "I wish I could",
+            "Amazing experience",
+            "Haha",
+            "Incredible",
+            "So relaxing",
+            "Beautiful",
+            "Great job"
+        };
+        List<string> _commentMiddles = new List<string>
+        {
+            "I’ve always wanted to try that",
+            "where did you go?",
+            "how did it go?",
+            "can you share more details?",
+            "sounds like a lot of fun",
+            "I need to do that too",
+            "hope I can join next time",
+            "looks so peaceful",
+            "that must have been exciting",
+            "I want to try this as well",
+            "so inspiring",
+            "that’s a good idea",
+            "sounds delicious",
+            "love this suggestion",
+            "can’t wait to try it"
+        };
+        List<string> _commentEnds = new List<string>
+        {
+            "😄",
+            "👍",
+            "💖",
+            "🌟",
+            "🎉",
+            "🙌",
+            "🍀",
+            "😊",
+            "👏",
+            "🔥",
+            "💡",
+            "🌸",
+            "✨",
+            "🎶",
+            "🌞",
+            "",
+            "",
+            "!",
+            ".",
+            ""
+        };
+        private List<PostComment> _comments = new List<PostComment>();
+        public PostCommentData(List<Post> posts, List<User> users)
+        {
+            int commentId = 1;
+            var random = new Random();
+            DateTime somedate = new DateTime(2020, 12, 05, 0, 0, 0, DateTimeKind.Utc);
+            foreach (var post in posts)
+            {
+                int numComments = random.Next(0, 5);
+
+                for (int i = 0; i < numComments; i++)
+                {
+                    var start = _commentStarts[random.Next(_commentStarts.Count)];
+                    var middle = _commentMiddles[random.Next(_commentMiddles.Count)];
+                    var end = _commentEnds[random.Next(_commentEnds.Count)];
+
+                    int commenterId;
+                    do
+                    {
+                        commenterId = users[random.Next(users.Count())].Id;
+                    } while (commenterId == post.UserId);
+
+                    _comments.Add(new PostComment
+                    {
+                        Id = commentId++,
+                        PostId = post.Id,
+                        UserId = commenterId,
+                        Content = $"{start}, {middle} {end}",
+                        CreatedAt = post.CreatedAt.AddMinutes(random.Next(5, 240)),
+                        UpdatedAt = null
+                    });
+                }
+            }
+        }
+        public List<PostComment> Comments { get { return _comments; } }
+
+    }
+}
---
+++ exercise.wwwapi/Data/PostData.cs
@@ -0,0 +1,88 @@
+﻿using exercise.wwwapi.Models;
+using System;
+
+namespace exercise.wwwapi.Data
+{
+    public class PostData
+    {
+        private List<string> _subject = new List<string>()
+        {
+            "Just finished",
+            "Trying out",
+            "Today I deployed",
+            "Learning",
+            "Exploring",
+            "Refactoring",
+            "Documented",
+            "Testing",
+            "Experimenting with",
+            "🔥"
+        };
+
+        private List<string> _objects = new List<string>
+        {
+            "my new development environment 🚀",
+            "Entity Framework Core",
+            "my first API to Azure 🎉",
+            "C# async patterns",
+            "LINQ queries",
+            "a machine learning project",
+            "a new UI for my app",
+            "a stock trading algorithm designed to cause HAVOC on the stock market",
+            "a new toy for my dog",
+            "a new feature request",
+            "🔥",
+            "a mystery novel",
+            "Master Chef UK",
+            "morning coffee",
+            "morning yoga",
+            "afternoon tea",
+            "chocolate chip cookies",
+        };
+
+        private List<string> _endings = new List<string>
+        {
+            "and it works perfectly!",
+            "which was surprisingly tricky.",
+            "can't wait to share it!",
+            "hoping to improve it soon.",
+            "finally solved a major bug 🐛",
+            "with some cool new tricks!",
+            "and learned a lot along the way.",
+            ", nothing works...",
+            "and im about to destroy my computer.",
+            ", my brain has ceased to exist.",
+            "🔥",
+            "and it was crazy!"
+        };
+        DateTime somedate = new DateTime(2020, 12, 05, 0, 0, 0, DateTimeKind.Utc);
+        private List<Post> _posts = new List<Post>();
+        private int numusers = 300;
+        public PostData(List<User> users)
+        {
+            Random random = new Random(1);
+            
+            for (int i = 1; i < 25; i++)
+            {
+                string subject = _subject[random.Next(9-1)];
+                string obj = _objects[random.Next(16-1)];
+                string ending = _endings[random.Next(12-1)];
+                string content = subject + " " + obj + " " + ending;
+                int likes = random.Next(0, 100);
+                int userid = random.Next(0, 100);
+
+                _posts.Add(new Post
+                {
+                    Id = i,
+                    UserId = users[random.Next(users.Count)].Id,
+                    Content = content,
+                    CreatedAt = somedate.AddDays(-i),
+                    UpdatedAt = null,
+                    NumLikes = likes
+                });
+            }
+        }
+
+        public List<Post> Posts { get { return _posts; } }
+    }
+}
