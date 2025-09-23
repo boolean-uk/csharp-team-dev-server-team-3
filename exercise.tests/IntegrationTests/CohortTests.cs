70fae81fb3ab887eea8d426f8dcad8f62d8a244c
Roman Eriksen <roman.eriksen@no.experis.com>
2025-09-23 09:20:46 +0200

    Removed old cohort test

--- exercise.tests/IntegrationTests/CohortTests.cs
+++ exercise.tests/IntegrationTests/CohortTests.cs
@@ -8,47 +8,4 @@ using System.Text;
 using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 
-namespace exercise.tests.IntegrationTests
-{
-    internal class CohortTests
-    {
-        private WebApplicationFactory<Program> _factory;
-        private HttpClient _client;
-
-        [SetUp]
-        public void SetUp()
-        {
-            // Arrange 
-            _factory = new WebApplicationFactory<Program>();
-            _client = _factory.CreateClient();
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            _client.Dispose();
-            _factory.Dispose();
-        }
-
-        [Test]
-        public async Task GetAllCohorts()
-        {
-            var response = await _client.GetAsync("/cohorts");
-            var contentString = await response.Content.ReadAsStringAsync();
-            var message = string.IsNullOrWhiteSpace(contentString) ? null : JsonNode.Parse(contentString);
-
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-            Assert.That(message, Is.Not.Null);
-            //Assert.That(message?["message"]?.GetValue<string>(), Is.EqualTo("success"));
-
-            var data = message?["data"]?.AsArray();
-            Assert.That(data, Is.Not.Null);
-            Assert.That(data!.Count, Is.GreaterThan(0));
-
-            var cohort = data!.First();
-            Assert.That(cohort["id"]?.GetValue<int>(), Is.GreaterThan(0));
-            Assert.That(cohort["title"].GetValue<string>, Is.Not.Null);
-            //Assert.That(cohort["courses"]["students"].GetValue<Array>, Is.Not.Null);
-        }
-    }
-}
+namespace exercise.tests.IntegrationTests{}
