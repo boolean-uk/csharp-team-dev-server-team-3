64334cefaf55b51857617d023fe4388ccdbf7320
Oyvind Timian Dokk Husveg <oyvind.timian.dokk.husveg@no.experis.com>
2025-09-25 13:51:41 +0200

    Added int cuz its used in some frontend scenarios

--- exercise.wwwapi/DTOs/UserDTO.cs
+++ exercise.wwwapi/DTOs/UserDTO.cs
@@ -5,6 +5,7 @@ namespace exercise.wwwapi.DTOs
 {
     public class UserDTO
     {
+        public int Id { get; set; }
         public string? Username { get; set; }
         public string? Email { get; set; }
         public string? FirstName { get; set; }
